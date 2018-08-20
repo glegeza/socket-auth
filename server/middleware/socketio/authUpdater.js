6833a6dfd6638eac54fb613ce7551156a1b93875
George Legeza <glegeza@gmail.com>
2018-08-20 14:45:23 -0400

    In progress authorization work

--- server/middleware/socketio/authUpdater.js
+++ server/middleware/socketio/authUpdater.js
@@ -2,7 +2,7 @@ module.exports = (socket, store) => {
     return (s, next) => {
         console.log('In auth updater');
         store.get(socket.request.sessionID, (err, session) => {
-            if (session.passport && session.passport.user) {
+            if (session && session.passport && session.passport.user) {
                 socket.has_auth = true;
             } else {
                 socket.has_auth = false;
